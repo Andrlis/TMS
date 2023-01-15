35737efe8bc003f10b0fda2b02ffb3341a9f1a92
Andrlis <andrlis0506@gmail.com>
2023-01-16 02:13:03 +0300

    [TMS][Lesson 7] Add printOperationHistory method

--- tms-lesson7/src/calculator/ConsoleApplication.java
+++ tms-lesson7/src/calculator/ConsoleApplication.java
@@ -15,18 +15,43 @@ public class ConsoleApplication implements Application {
 
 	public void run() {
 		while (true) {
-			writer.write("Enter num 1");
-			double num1 = reader.readDouble();
-			writer.write("Enter num 2");
-			double num2 = reader.readDouble();
-			writer.write("Enter operation type");
-			String type = reader.readString();
-			Operation op = new Operation(num1, num2, type);
-			Operation result = calculator.calculate(op);
-			storage.save(result);
-			writer.write("Result = " + result.result);
-			writer.write("");
-			Operation[] all = storage.findAll();
+			showMenu();
+			String userInput = reader.readString();
+			switch (userInput){
+				case "0":
+					return;
+				case "1":
+					writer.write("Enter num 1");
+					double num1 = reader.readDouble();
+					writer.write("Enter num 2");
+					double num2 = reader.readDouble();
+					writer.write("Enter operation type");
+					String type = reader.readString();
+					Operation op = new Operation(num1, num2, type);
+					Operation result = calculator.calculate(op);
+					storage.save(result);
+					writer.write("Result = " + result.getResult());
+					writer.write("");
+					break;
+				case "2":
+					printOperationHistory(storage.findAll());
+					break;
+				default:
+					writer.write("Unsupported operation");
+			}
+
+		}
+	}
+
+	private void showMenu() {
+		writer.write("Please, choose an option:\n1 - Calculate\n2 - Show history\n0 - Finish");
+	}
+
+	private void printOperationHistory(Operation[] operations) {
+		for (Operation operation: operations){
+			if(operation != null) {
+				writer.write(operation.toString());
+			}
 		}
 	}
 }
